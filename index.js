c54ee561de89045a771f245b30ce890c300a2cb8
Ekaterina <ekaterina@MacBook-Pro-de-Ekaterina.local>
2021-05-19 14:57:15 +0200

    request of route

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ const formidable = require('express-formidable');
 const mongoose = require("mongoose");
 
 
-app.use(express);
+const app = express();
 app.use(formidable());
+
+const userRoutes = require('./routes/user');
+app.use(userRoutes);
+
+mongoose.connect("mongodb://localhost:27017/airbnb", {
+    useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true
+});
+
+app.listen(3000, () => {
+    console.log("Server has started")
+}); 
